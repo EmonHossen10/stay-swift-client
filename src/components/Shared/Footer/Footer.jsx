2431813fb408e60a115d43ae9491d32ff54ef13e
unknown <emonhossen1000@gmail.com>
2024-02-08 22:43:52 +0600

    first commit

---
+++ src/components/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='px-4 divide-y   bg-gray-600 relative bottom-0 left-0'>
+      <div className='py-6 text-sm text-center  text-white'>
+        © 2023 SwiftStay Inc. All rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
